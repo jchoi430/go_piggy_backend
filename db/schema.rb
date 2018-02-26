ec03598d51d0883db2803c632d789b2d42c727f7
jchoi201 <jae.choi@fullscreen.net>
2018-02-25 22:04:13 -0800

    add product language

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226053110) do
+ActiveRecord::Schema.define(version: 20180226060150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180226053110) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "sales_rank"
+    t.string "product_language"
   end
 
   create_table "ama_product_list_prices", force: :cascade do |t|
